614e9dcbc5b0ddcc9812a8579ea356df55430eb0
percytrar <prashant.trar97@yahoo.in>
2020-06-22 00:31:58 +0530

    Updates

---
+++ precache-manifest.dc7720e8fe8adfdd2f7fc08df570d5d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32424a558da99767497d2224383f21ef",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "6745bb15fec8794fa393",
+    "url": "/CoronaTracker/static/css/main.da561392.chunk.css"
+  },
+  {
+    "revision": "9ad254cd497228c66424",
+    "url": "/CoronaTracker/static/js/2.52f493b8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoronaTracker/static/js/2.52f493b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6745bb15fec8794fa393",
+    "url": "/CoronaTracker/static/js/main.6517acc1.chunk.js"
+  },
+  {
+    "revision": "c3c44d45839e2a10eb73",
+    "url": "/CoronaTracker/static/js/runtime-main.e98b620d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoronaTracker/static/media/image.d7265326.png"
+  }
+]);
